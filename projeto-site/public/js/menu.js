f8980542fa6d911b0475abe00ecd53081e7a27c8
fekling <felipe.kling@bandtec.com.br>
2020-06-24 22:48:45 -0300

    att

--- projeto-site/public/js/menu.js
+++ projeto-site/public/js/menu.js
@@ -1,11 +1,9 @@
 function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
-    document.getElementById("alert").style.marginLeft = "50px";
   }
 
   function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
-    document.getElementById("alert").style.marginLeft = "30px";
   }
